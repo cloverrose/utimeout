c4e53ce74059dae3723b30bbe64c396bc2d6f87a
cloverrose <clover.plus.rose@gmail.com>
2013-11-09 23:03:25 +0900

    move setting of stdout/stderr into start function

--- utimeout.py
+++ utimeout.py
@@ -47,47 +47,41 @@ def _calc_total_cputime(r, tick, redis_key):
     return running_child_cputime + times[2] + times[3]
 
 
-def _start_core(cmd, timeout, polling_time=1, verbose=False, stdout=None, stderr=None):
-    out = stdout if stdout is not None else sys.stdout
-    if stderr == STDOUT:
-        err = out
-    else:
-        err = stderr if stderr is not None else sys.stderr
-
+def _start_core(cmd, timeout, polling_time=1, verbose, stdout, stderr):
     redis_key = uuid.uuid4().hex
     os.environ['redis_key'] = redis_key
 
     tick = _get_tick()
     if verbose:
-        err.write('tick: {0}\n'.format(tick))
-        err.flush()
+        stderr.write('tick: {0}\n'.format(tick))
+        stderr.flush()
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     r.delete(redis_key)
 
-    p = Popen(cmd, preexec_fn=os.setsid, stderr=subprocess.STDOUT, stdout=out)
+    p = Popen(cmd, preexec_fn=os.setsid, stderr=subprocess.STDOUT, stdout=stdout)
 
     while p.poll() is None:
         cputime = _calc_total_cputime(r, tick, redis_key)
         if verbose:
-            err.write('cputime: {0}s\n'.format(cputime))
-            err.flush()
+            stderr.write('cputime: {0}s\n'.format(cputime))
+            stderr.flush()
         if cputime > timeout:
             os.killpg(p.pid, signal.SIGTERM)
-            err.write('timeout ({0}s)\n'.format(cputime))
-            err.flush()
+            stderr.write('timeout ({0}s)\n'.format(cputime))
+            stderr.flush()
             timeout = True
             break
         time.sleep(polling_time)
     else:
         cputime = _calc_total_cputime(r, tick, redis_key)
-        err.write('finish ({0}s)\n'.format(cputime))
-        err.flush()
+        stderr.write('finish ({0}s)\n'.format(cputime))
+        stderr.flush()
         timeout = False
     r.delete(redis_key)
     return timeout
 
 
-def _start_queue(queue, cmd, timeout, polling_time=1, verbose=False, stdout=None, stderr=None):
+def _start_queue(queue, cmd, timeout, polling_time=1, verbose, stdout, stderr):
     timeout = _start_core(cmd, timeout, polling_time, verbose, stdout, stderr)
     queue.put(timeout)
 
@@ -97,6 +91,12 @@ def start(cmd, timeout, polling_time=1, verbose=False, stdout=None, stderr=None)
     if timeout return True,
     if finish return False.
     """
+    out = stdout if stdout is not None else sys.stdout
+    if stderr == STDOUT:
+        err = out
+    else:
+        err = stderr if stderr is not None else sys.stderr
+
     queue = Queue()
     p = Process(target=_start_queue,
                 args=(queue,),
@@ -104,8 +104,8 @@ def start(cmd, timeout, polling_time=1, verbose=False, stdout=None, stderr=None)
                             timeout=timeout,
                             polling_time=polling_time,
                             verbose=verbose,
-                            stdout=stdout,
-                            stderr=stderr))
+                            stdout=out,
+                            stderr=err))
     p.start()
     timeout = queue.get()
     p.join()
