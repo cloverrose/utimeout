b3fc5a8b9466eba7f99318a0ba52c178e68cad51
cloverrose <clover.plus.rose@gmail.com>
2013-11-09 21:56:31 +0900

    delete demo_concurrent.py which call start as process, and utimeout stop supporting Celery provisionally (delete tasks.py and demo_queue.py)

--- sample/demo_queue.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-from tasks import start
-
-TIMEOUT = 30
-
-if __name__ == '__main__':
-    for i in range(3):
-        start.delay(['time', 'python', 'fibwrapper.py', '20', '6', 'name{0}'.format(i)], TIMEOUT)
